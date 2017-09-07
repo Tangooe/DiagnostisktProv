530f72f3eebc2b6c34ca2db8448c42af1f6adec8
Emil Ekman <tangooe@gmail.com>
2017-09-07 11:18:29 +0200

    Added DataAnnotations to Product model

--- DiagnostisktProv/Models/Product.cs
+++ DiagnostisktProv/Models/Product.cs
@@ -1,9 +1,15 @@
-﻿namespace DiagnostisktProv.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiagnostisktProv.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
+
+        [Range(5,1000)]
         public decimal Price { get; set; }
     }
 }
